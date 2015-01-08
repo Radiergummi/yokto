4530589558d2edb37d073f969411b9483ad67354
Mo <Radiergummi@users.noreply.github.com>
2015-01-08 18:13:53 +0100

    Update config.php

--- system/config.php
+++ system/config.php
@@ -18,6 +18,9 @@ define('SITENAME', 'Website name');
 // template folder path
 define('TDIR', 'templates' . DS);
 
+// template folder path
+define('LOGDIR', SYS . 'log' . DS);
+
 // available routes for this site.
 // every route has its own array which is extendable (like the functionally unecessary comments-enabled)
 $routes = array(
