5f9d05d24d35f211cf4992a0a5fac5c16929da55
Mo <Radiergummi@users.noreply.github.com>
2015-01-08 15:49:47 +0100

    Update functions.php

--- system/functions.php
+++ system/functions.php
@@ -18,12 +18,34 @@
  * @param string $type the template name for this type of page.
  * @param mixed $id the unique identifier of the requested page.
  */
-function render_view($route, $request = '') {
+function render_view($route, $request) {
 	
 	// import template variables and error messages
-	global $template_vars, $errors;
+	global $template_vars;
+
+
+	if ($route === 'error') {
+
+		// append to log channel messages
+		syslog('Error 404 - Page not found: "' . URL . DS . $request . '" requested from client ' . $_SERVER['REMOTE_ADDR'] . '.');
+
+		// send correct headers
+		header('Status: 404 Not Found');
+
+		// include error 404-template
+		$content = get_template('http_404');
+
+	} else {
+		// if we have a request string and valid route, set title to specific string
+		$template_vars['{$title}'] = ((!empty($request)) && (!empty($route)) ? SITENAME . ' | ' . $route['name'] : SITENAME);
+		$template_vars['{$name}'] = $route['name'];
+		
+		$content = get_template($route['template']);
+
+	}
 	
-	$content = get_template($route['template']);
+
+
 
 	switch($route['template']) {
 		case 'index':
@@ -31,8 +53,6 @@ function render_view($route, $request = '') {
 		break;
 
 		case 'error':
-			header('Status: 404 Not Found');
-			$content = '<div id="error">' . $errors['http_404'] . '</div>';
 		break;
 	}
 	$htmlheader = get_template('inc_header');
@@ -50,13 +70,28 @@ function render_view($route, $request = '') {
  * @return the content of the template file or an appropriate error message.
  */
 function get_template($name) {
-	global $error;
 	$template;
 	$path = PATH . TDIR . $name . EXT;
 	if (file_exists($path)) {
 		$template = file_get_contents($path);
 	} else {
-		$template = '<div id="error">' . $errors['template_not_found'] . '</div>';
+		$error_message = 'The requested template could not be retrieved. Please check your routes array in the config file.';
+		if (!LOG){
+			$template = '<div id="error">' . $error_message . '</div>';
+		} else {
+			syslog($error_message,'3');
+			$template = '<div id="error">Oops! There went something wrong. Please notify the owner of this site.</div>';
+		}
 	}
 	return $template;
+}
+/**
+ * appends a message to the log. 
+ * 
+ * @param string $message the message to log
+ * @param int $channel [1,2,3] the channel to log to, defaults to 1 = messages.
+ */
+function syslog($message,$channel = 1) {
+	$file = array('messages','debug','error');
+	error_log(date('r',$time) . ': ' . $message . PHP_EOL, 3, SYS . $file[$channel] . '.log');
 }
