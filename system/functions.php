a0401f7bc132ce165e14c12e7ef6d598005cdd07
Mo <Radiergummi@users.noreply.github.com>
2015-01-08 21:47:12 +0100

    Update functions.php

--- system/functions.php
+++ system/functions.php
@@ -56,7 +56,7 @@ function render_view($route, $request) {
  */
 function get_template($name) {
 	$template;
-	$path = PATH . TPLDIR . $name . EXT;
+	$path = PATH . TDIR . $name . EXT;
 	if (file_exists($path)) {
 		$template = file_get_contents($path);
 	} else {
@@ -89,26 +89,23 @@ function syslog($message,$channel = 1) {
  * 
  * @param mixed $data the variable to dump
  */
- function debug($data = 'no variable given') {
-	$debug = debug_backtrace();
+ function debug($data = 'no data given') {
+	$debug = array_reverse(debug_backtrace());
 	$trace;
     foreach ($debug as $k => $v) { 
-		if ($k < 1) { 
-            continue;
-		} 
         array_walk($v['args'], function (&$item, $key) { 
             $item = var_export($item, true); 
         }); 
 
-        $trace .= '#' . ($k - 1) . '    ' . $v['file'] . '(' . $v['line'] . '): ' . (isset($v['class']) ? $v['class'] . '->' : '') . $v['function'] . '(' . implode(', ', $v['args']) . ')' . "\n"; 
+        $trace .= '<div><span>#' . $k . '    ' . $v['file'] . ' (<b>' . $v['line'] . '</b>):</span> ' . (isset($v['class']) ? $v['class'] . '->' : '') . $v['function'] . '(' . implode(', ', $v['args']) . ')' . "</div>\n"; 
     } 
-	echo '<style>pre{margin:1rem;padding:1rem;background:#f2f2f2;border:1px solid #c0c0c0;border-radius:3px;box-shadow:0 1px 5px rgba(0,0,0,.2)}h1{display:block;margin:0 0 20px;padding:0 0 .5rem;border-bottom:1px solid #ccc}.info_self{float:right;padding:4px 10px;background:#fff;border-radius:3px;border:1px solid #ccc;}.dump{margin:5px 0;padding:5px 1rem;background:#eee;border:1px solid #ccc;border-radius:3px;}.dump>h3{margin:5px 0}</style>';
+	echo '<style>pre{margin:1rem;padding:1rem;background:#fafafa;border:1px solid #c0c0c0;border-radius:3px;box-shadow:0 1px 5px rgba(0,0,0,.2);white-space:pre-line}h1{display:block;margin:0 0 20px;padding:0 0 .5rem;border-bottom:1px solid #ccc}.info_self{float:right;padding:4px 10px;background:#BCFF95;border-radius:3px;border:1px solid #ccc;}.dump{margin:5px 0;padding:5px 1rem;background:#eee;border:1px solid #ccc;border-radius:3px;}.dump>h3{margin:5px 0}.trace{margin-top:1rem;padding-left:22px;line-height:calc(1rem + 4px);text-indent:-22px;word-wrap:break-word}.trace>div:last-of-type{color:#999}.trace>div:last-of-type>span{background:rgba(100,200,105,.44);color:#555}.trace span{padding:2px 6px;background:rgba(31,20,218,.22);border-radius:3px}</style>';
 	echo '<pre><div class="info_self"> Halted at line ' . $debug[0][line] . '.</div><h1>Debug Info:</h1>';
 	echo '';
-	echo '<div class="dump"><h3>Dumped variable ' . $$data . ':</h3>';
-	print_r($data . PHP_EOL . PHP_EOL);
-	echo '</div>';
+	echo '<div class="dump"><h3>Dumped ' . gettype($data) . ':</h3>';
+	print_r(' " ' . $data . ' " ' . PHP_EOL . PHP_EOL);
+	echo '</div><div class="trace">';
 	print_r($trace);
-	echo '</pre>';
+	echo '</div></pre>';
 	exit();
  }
