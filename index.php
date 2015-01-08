740be1087267181ba604c13eb465556cba7b4530
Mo <Radiergummi@users.noreply.github.com>
2015-01-08 11:12:59 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?
+/**
+ * picoFrame v0.2
+ *
+ * minimalist website framework designed for the most basic web apps and sites
+ *
+ * @package		picoFrame
+ * @link		http://moritzfriedrich.com
+ * @copyright	http://unlicense.org/
+ */
+
+define('DS', DIRECTORY_SEPARATOR);
+define('URL', (isset($_SERVER['HTTPS']) ? "https" : "http") . '://' . $_SERVER['HTTP_HOST']);
+
+define('PATH', dirname(__FILE__) . DS);
+define('SYS', PATH . 'system' . DS);
+define('EXT', '.php');
+
+require SYS . 'config' . EXT;
+require SYS . 'hooks' . EXT;
+require SYS . 'functions' . EXT;
+require SYS . 'router' . EXT;
