1089a5749f7bb38cb2805776928fbb45aa3c4528
Mo <Radiergummi@users.noreply.github.com>
2015-01-09 00:56:53 +0100

    Update inc_header.php

--- templates/inc_header.php
+++ templates/inc_header.php
@@ -1,20 +1,21 @@
 <!Doctype html>
 <html>
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#">
-	<title>{$title}</title>
+	<title><?=$title?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="description" content="text">
 	<meta name="keywords" content="text">
 	<meta name="web_author" content="Moritz Friedrich">
 
-	<meta property="og:title" content="{$title}">
-	<meta property="og:image" content="{$siteurl}/{$assetdir}/img/logo.png" />
+	<meta property="og:title" content="<?=$title?>">
+	<meta property="og:image" content="<?=$siteurl?>/<?=$assetdir?>/img/logo.png" />
 	<meta property="og:site_name" content="EXAMPLE" />
 	<meta property="og:description" content="DESCRIPTION" />
 
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
 
-	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto" />
-	<link rel="stylesheet" href="{$siteurl}/{$assetdir}/css/style.css" />
+	<link rel="stylesheet" href="<?=$siteurl?>/<?=$assetdir?>/css/reset.min.css" />
+	<link rel="stylesheet" href="<?=$siteurl?>/<?=$assetdir?>/css/modulo.lib.css" />
+	<link rel="stylesheet" href="<?=$siteurl?>/<?=$assetdir?>/css/style.css" />
 </head>
 <body ontouchstart="">
