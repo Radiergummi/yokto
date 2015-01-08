837ef52db1f00f7129833f307920cc8889e74269
Mo <Radiergummi@users.noreply.github.com>
2015-01-08 14:31:28 +0100

    Update router.php

--- system/router.php
+++ system/router.php
@@ -16,16 +16,4 @@ $request = ltrim(htmlspecialchars($_SERVER['REQUEST_URI']),'/');
 // if the request is contained in our routes array in config, set route
 $route = (array_key_exists($request,$routes) ? $request : 'error');
 
-// if we have a request string and valid route, set title to specific string
-$template_vars['{$title}'] = ((!empty($request)) && (!empty($route)) ? SITENAME . ' | ' . $routes[$route]['title'] : SITENAME);
-
-// if route name is in available routes array, show content
-if ($route != 'error') {
-	$template_vars['{$name}'] = $routes[$route]['title'];
-	render_view($routes[$route]);
-}
-
-// else, throw error 404
-else {
-	render_view('error', $request);
-}
+render_view($routes[$route], $request);
