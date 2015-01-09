3fa9c97a533d0749077d90eb04cdc329d3ba2c13
Mo <Radiergummi@users.noreply.github.com>
2015-01-09 22:26:40 +0100

    Update view.php

--- system/view.php
+++ system/view.php
@@ -32,7 +32,7 @@ class View {
 	 *
 	 * @var array
 	 */
-	public $vars = array(
+	public static $vars = array(
 		'title' => SITENAME,
 		'siteurl' => URL,
 		'assetdir' => 'assets'
@@ -44,57 +44,20 @@ class View {
 	 * @param array
 	 * @param string
 	 */
-	public function __construct($route, $request) {
+	public function __construct($route) {
 		$this->route = $route;
-		$this->request = $request;
+		$this->request = $route['request'];
 		$this->vars = array_merge($this->vars, $route['variables']);
-	}
-	
-	/**
-	 * prepares the html content without header and footer for easy inline inclusion
-	 * 
-	 * @param array
-	 * @param string
-	 */
-	public function render_ajax($route, $request) {
-		$route = $this->route;
-		$request = $this->request;
-		
-		if ($route['slug'] === 'error') {
-
-			// append to log channel messages
-			syslog('Error 404 - Page not found: "' . URL . DS . $request . '" requested from client ' . $_SERVER['REMOTE_ADDR'] . '.', 0);
-
-			// set correct headers
-			header('Status: 404 Not Found');
-
-		}
-
-			// set title to sitename only for index, append site name to other pages
-			$this->vars['title'] = ((!empty($request)) && (!empty($route)) ? SITENAME . ' | ' . $route['name'] : SITENAME);
-			$this->vars['name'] = $route['name'];
-
-
-		ob_start();
-		
-		extract($this->vars);
-		
-		require TPLDIR . $route['template'] . EXT;
-
-		return ob_get_contents();
-
 	}
 	
 	/**
 	 * prepares the html content and delivers it to the client.
 	 * 
-	 * @param array
-	 * @param string
+	 * @param boolean
 	 */
-	function render() {
-		$route = $this->route;
-		$request = $this->request;
-
+	public static function render($route, $ajax = false) {
+		$request = $route['request'];
+		$vars = array_merge(self::$vars, $route['variables']);
 		if ($route['slug'] === 'error') {
 
 			// append to log channel messages
@@ -102,21 +65,21 @@ class View {
 
 			// set correct headers
 			header('Status: 404 Not Found');
-			
 		}
 
 		// set title to sitename only for index, append site name to other pages
-		$this->vars['title'] = ((!empty($request)) && (!empty($route)) ? SITENAME . ' | ' . $route['name'] : SITENAME);
-		$this->vars['name'] = $route['name'];
+		$vars['title'] = ((!empty($request)) && (!empty($route)) ? SITENAME . ' | ' . $route['name'] : SITENAME);
+		$vars['name'] = $route['name'];
+		$vars['slug'] = $route['slug'];
 
 		ob_start();
 		
-		extract($this->vars);
+		extract($vars);
 		
-		require TPLDIR . 'inc_header' . EXT;
+		if (!$ajax) require TPLDIR . 'inc_header' . EXT;
 		require TPLDIR . $route['template'] . EXT;
-		require TPLDIR . 'inc_footer' . EXT;
+		if (!$ajax) require TPLDIR . 'inc_footer' . EXT;
 		
-		return ob_get_contents();
+		return ob_get_clean();
 	}
 }
