ea9bd7d7e1aa3d2e32aea00960244771c6310ba4
Mo <Radiergummi@users.noreply.github.com>
2015-01-09 22:29:02 +0100

    Update router.php

--- system/router.php
+++ system/router.php
@@ -9,22 +9,63 @@
  * @copyright	http://unlicense.org/
  */
 
-// the checked route
-$valid;
- 
-// the request string is exploded into requested nodes to support sub categories in the future
-$request = explode('/',ltrim(htmlspecialchars($_SERVER['REQUEST_URI']),'/'));
 
-// if the request is contained in our routes array in config, set route
-foreach ($routes as $route => $meta) {
-	if ($request[0] === $meta['slug']) {
-		$valid = $routes[$route]; // if correct route is found, return the array containing its meta info
-		break;
-	} else {
-		$valid = $routes[0]; // if no route is found, return the array for our error page
+class Router {
+	
+	/**
+	 * the available routes
+	 *
+	 * @var array
+	 */
+	protected $routes;
+
+	/**
+	 * the verified route
+	 *
+	 * @var array
+	 */
+	protected $match;
+
+	/**
+	 * Constructor
+	 *
+	 * @param array
+	 * @param string
+	 */
+	public function __construct($routes) {
+		$this->routes = $routes;
+	}
+
+	/**
+	 * match the request to an existing route
+	 *
+	 * @param array
+	 * @param string
+	 */
+	public function match($request) {
+
+		// the request string is exploded into requested nodes to support sub categories in the future
+		$request = explode('/',ltrim(htmlspecialchars($_SERVER['REQUEST_URI']),'/'));
+
+		$routes = $this->routes;
+
+		// if the request is contained in our routes array in config, set route
+		foreach ($routes as $route => $meta) {
+			if ($request[0] === $meta['slug']) {
+
+				// if correct route is found, return the array containing its meta info
+				$routes[$route]['request'] = $request;
+				$this->match = $routes[$route];
+
+				break;
+
+			} else {
+
+				// if no route is found, return the array for our error page
+				$this->match = $routes[0];
+			}
+		}
+		
+		return $this->match;
 	}
 }
-
-$view = new View($valid, $request);
-
-$view->render();
