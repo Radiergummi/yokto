153b7acc88380162d7b5ba2fa65cc080e5138a4d
Mo <Radiergummi@users.noreply.github.com>
2015-01-09 09:29:02 +0100

    Update router.php

--- system/router.php
+++ system/router.php
@@ -1,10 +1,10 @@
 <? namespace System;
 /**
- * pico
+ * yokto
  *
  * minimalist website framework designed for the most basic web apps and sites
  *
- * @package		pico
+ * @package		yokto
  * @link		http://moritzfriedrich.com
  * @copyright	http://unlicense.org/
  */
