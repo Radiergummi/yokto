2bd139c6186dfd63e44f75a072e84abc6e6b6f0c
Mo <Radiergummi@users.noreply.github.com>
2015-01-09 10:20:47 +0100

    Update router.php

--- system/router.php
+++ system/router.php
@@ -9,15 +9,15 @@
  * @copyright	http://unlicense.org/
  */
 
-
+// the checked route
 $valid;
  
-// the request string
-$request = ltrim(htmlspecialchars($_SERVER['REQUEST_URI']),'/');
+// the request string is exploded into requested nodes to support sub categories in the future
+$request = explode('/',ltrim(htmlspecialchars($_SERVER['REQUEST_URI']),'/'));
 
 // if the request is contained in our routes array in config, set route
 foreach ($routes as $route => $meta) {
-	if ($request === $meta['slug']) {
+	if ($request[0] === $meta['slug']) {
 		$valid = $routes[$route]; // if correct route is found, return the array containing its meta info
 		break;
 	} else {
