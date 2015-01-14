32c12d74ef92ac024f463128c807d57ef128a491
Mo <Radiergummi@users.noreply.github.com>
2015-01-14 09:13:38 +0100

    Create routes.php

---
+++ system/config/routes.php
@@ -0,0 +1,15 @@
+<?
+return array(
+	array(
+		'slug' => 'error',
+		'name' => 'Error: Page not found',
+		'template' => 'http_404',
+	),
+	array(
+		'slug' => '',
+		'template' => 'page_start'
+	),
+	array(
+		'slug' => 'static-example-route'
+	)
+);
