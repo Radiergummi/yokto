853ec066b4c230db4a8cd40c9bd05cabc90a673d
Mo <Radiergummi@users.noreply.github.com>
2015-01-14 09:12:10 +0100

    Update config.php

--- system/config.php
+++ system/config.php
@@ -9,58 +9,56 @@
  * @copyright	http://unlicense.org/
  */
 
-
 // enable or disable logging
 define('LOG', false);
- 
-// site name
-define('SITENAME', 'Yokto test instance');
+
 // template folder path
-define('TPLDIR', PATH . 'templates' . DS);
+define('TPLDIR', PATH . 'themes' . DS . 'kappa' . DS);
+
 // log folder path
 define('LOGDIR', SYS . 'log' . DS);
 
 // available routes for this site
-$routes = array(
-	array(
-		'slug' => 'error',
-		'name' => 'Error: Page not found',
-		'template' => 'http_404',
-		'comments-enabled' => false,
-		'variables' => array(
-			'hello_world' => Hook::hello_world(),
-			'{$foo}' => 'bar'
-		)
-	),
-	array(
-		'slug' => '',
-		'name' => 'Home',
-		'template' => 'page_start',
-		'comments-enabled' => false,
-		'variables' => array(
-			'hello_world' => Hook::hello_world(),
-			'intro' => 'This is the first page visitors get to see.',
-			'jQuery' => true
-		)
-	),
-	array(
-		'slug' => 'foo-bar',
-		'name' => 'foo bar!',
-		'template' => 'page',
-		'comments-enabled' => true,
-		'variables' => array(
-			'hello_world' => Hook::hello_world(),
-			'{$foo}' => 'bar'
-		)
-	),
-	array(
-		'slug' => 'portfolio',
-		'name' => 'Example Page',
-		'template' => 'page_portfolio',
-		'comments-enabled' => false,
-		'variables' => array(
-			'hello_world' => Hook::hello_world(),
-			'{$foo}' => 'bar'
-		)
-	)
-);
+
+$routes = Config::routes();
+
+class Config {
+	/**
+	 * holds the configuration
+	 * 
+	 * @var array
+	 */
+	public static $data = array();
+	
+	public static function set($file, $key, $value){
+			static::$data[$file][$key] = $value;
+	}
+	
+	public static function get($file, $value){
+		if(is_readable($path = SYS . 'config' . DS . $file . EXT)) {
+			static::$data[$file] = require $path;
+		}
+		
+		return (isset($value) ? self::$data[$file][$value] : self::$data[$file]);
+	}
+	
+	/**
+	 * Returns a value from the config array using the
+	 * method call as the array key reference
+	 *
+	 * @example Config::app('url');
+	 *
+	 * @param string
+	 * @param array
+	 *
+	 * @return string|array
+	 */
+	public static function __callStatic($method, $arguments = '') {
+		$key = $method;
+		
+		$value = (is_array($arguments) && ($arguments > 1) ? array_shift($arguments) : (string) $arguments);
+
+		return self::get($key, $value);
+	}
+	
+}
