d6cd4af8ec8e05e34431782a9360451f3eb4225d
Mo <Radiergummi@users.noreply.github.com>
2015-01-14 11:03:30 +0100

    Update inc_header.php

--- themes/kappa/inc_header.php
+++ themes/kappa/inc_header.php
@@ -3,14 +3,14 @@
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#">
 	<title><?=$title?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="description" content="text">
-	<meta name="keywords" content="text">
+	<meta name="description" content="Yokto test site">
+	<meta name="keywords" content="This is a demo page to showcase Yokto, an open source CMS for developers.">
 	<meta name="web_author" content="Moritz Friedrich">
 
 	<meta property="og:title" content="<?=$title?>">
 	<meta property="og:image" content="<?=theme_url('assets')?>/img/logo.png" />
-	<meta property="og:site_name" content="EXAMPLE" />
-	<meta property="og:description" content="DESCRIPTION" />
+	<meta property="og:site_name" content="Yokto test instance" />
+	<meta property="og:description" content="This is a demo page to showcase Yokto, an open source CMS for developers." />
 
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
 
@@ -52,3 +52,4 @@
 			</ul>
 		</nav>
     </header>
+	<main>
