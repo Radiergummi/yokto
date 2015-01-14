8d4cd8b9c8dcf6f6aadf5b80332a6c7e493ae2c4
Mo <Radiergummi@users.noreply.github.com>
2015-01-14 09:18:32 +0100

    Create inc_footer.php

---
+++ themes/kappa/inc_footer.php
@@ -0,0 +1,10 @@
+	<script src="//code.jquery.com/jquery.min.js"></script>
+	<script>
+		$(document).ready(function(){
+			$('#themeSwitcher').click(function(){
+				$('body').toggleClass('night');
+			});
+		});
+	</script>
+  </body>
+</html>
