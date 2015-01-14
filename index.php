495d1b4fdd7c3b79aa5f4e95cf521853a74b1ec1
Mo <Radiergummi@users.noreply.github.com>
2015-01-14 12:15:16 +0100

    Removed ENV constant

--- index.php
+++ index.php
@@ -9,15 +9,12 @@
  * @copyright	http://unlicense.org/
  */
 
-// controls logging, available values: development, testing, production
-define('ENV', 'development');
- 
 define('DS', DIRECTORY_SEPARATOR);
-define('URL', (isset($_SERVER['HTTPS']) ? "https" : "http") . '://' . $_SERVER['HTTP_HOST']);
-
 define('PATH', dirname(__FILE__) . DS);
 define('SYS', PATH . 'system' . DS);
 define('EXT', '.php');
+
+define('URL', (isset($_SERVER['HTTPS']) ? "https" : "http") . '://' . $_SERVER['HTTP_HOST']);
 
 require SYS . 'hooks' . EXT;
 require SYS . 'config' . EXT;
