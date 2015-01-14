ab12961de1366e5bfd985bcff139623f9f12b2f9
Mo <Radiergummi@users.noreply.github.com>
2015-01-14 09:16:36 +0100

    Create page.php

---
+++ themes/kappa/page.php
@@ -0,0 +1,9 @@
+<main>
+	<article class="page">
+		<header>
+			<h1>Example Site</h1>
+		</header>
+		<p>Below is some example content written with markdown to give an impression of how things work:</p>
+		<p><?=site_text($slug)?></p>
+	</article>
+</main>
