46a79c2550c35199eb87f6350941bc2a0ec02a43
Mo <Radiergummi@users.noreply.github.com>
2015-01-14 09:06:54 +0100

    Update view.php

--- system/view.php
+++ system/view.php
@@ -18,11 +18,7 @@ class View {
 	 *
 	 * @var array
 	 */
-	public static $vars = array(
-		'title' => SITENAME,
-		'siteurl' => URL,
-		'assetdir' => 'assets'
-	);
+	public static $vars = array();
 
 	/**
 	 * prepares the html content and delivers it to the client.
@@ -30,30 +26,54 @@ class View {
 	 * @param boolean
 	 */
 	public static function render($route, $ajax = false) {
-		$request = $route['request'];
-		$vars = array_merge(self::$vars, $route['variables']);
-		if ($route['slug'] === 'error') {
+		$request = Config::routes('request');
+
+		#if ($route['slug'] === 'error') {
+		if ($route === 'error') {
 
 			// append to log channel messages
-			syslog('Error 404 - Page not found: "' . URL . DS . $request . '" requested from client ' . $_SERVER['REMOTE_ADDR'] . '.', 0);
+			log('Error 404 - Page not found: "' . URL . DS . $request . '" requested from client ' . $_SERVER['REMOTE_ADDR'] . '.', 0);
 
 			// set correct headers
 			header('Status: 404 Not Found');
+			
+			// set error template
+			$template = 'http_404';
+			
+		} else {
+			$template = (!empty($route['template']) ? $route['template'] : 'page');
 		}
 
-		// set title to sitename only for index, append site name to other pages
-		$vars['title'] = ((!empty($request)) && (!empty($route)) ? SITENAME . ' | ' . $route['name'] : SITENAME);
+		$theme = self::get_theme();
+		$vars = (!empty($route['variables']) ? array_merge(self::$vars, $route['variables']) : self::$vars);
+
+		$vars['sitename'] = Config::env('sitename');
+		$vars['title'] = ((!empty($request)) && (!empty($route)) ? $sitename . ' | ' . $route['name'] : $sitename);
 		$vars['name'] = $route['name'];
 		$vars['slug'] = $route['slug'];
-
+		// start collecting the output
 		ob_start();
 		
-		extract($vars);
+			extract($vars);
+			require SYS . 'theme_functions'. EXT;
 		
-		if (!$ajax) require TPLDIR . 'inc_header' . EXT;
-		require TPLDIR . $route['template'] . EXT;
-		if (!$ajax) require TPLDIR . 'inc_footer' . EXT;
+			if (!$ajax) require TPLDIR . 'inc_header' . EXT;
+			#require TPLDIR . $route['template'] . EXT;
+			require TPLDIR . $template . EXT;
+			
+			if (!$ajax) require TPLDIR . 'inc_footer' . EXT;
 		
 		return ob_get_clean();
+	}
+	
+	public static function get_theme(){
+		if (is_readable($file = PATH . 'themes' . DS . Config::env('theme') . DS . 'theme.json')){
+			return json_decode(file_get_contents($file),true);
+		} else {
+			Error::exception('Theme file not found at ' . $file);
+
+			// append to log channel error
+			log('Error 404 - Page not found: "' . URL . DS . $request . '" requested from client ' . $_SERVER['REMOTE_ADDR'] . '.', 2);
+		}
 	}
 }
