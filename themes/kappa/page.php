5673f384621909095aaa41ca84449321743ae8e3
Mo <Radiergummi@users.noreply.github.com>
2015-01-14 11:03:51 +0100

    Update page.php

--- themes/kappa/page.php
+++ themes/kappa/page.php
@@ -1,9 +1,3 @@
-<main>
-	<article class="page">
-		<header>
-			<h1>Example Site</h1>
-		</header>
-		<p>Below is some example content written with markdown to give an impression of how things work:</p>
-		<p><?=site_text($slug)?></p>
-	</article>
-</main>
+<article class="page">
+	<p><?=site_text($slug)?></p>
+</article>
