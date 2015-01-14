0bc833435c5c2c61aa7b7f7ea61d5eaf5dd9df98
Mo <Radiergummi@users.noreply.github.com>
2015-01-14 11:02:05 +0100

    Update http_404.php

--- themes/kappa/http_404.php
+++ themes/kappa/http_404.php
@@ -1,10 +1,7 @@
-<div id="error"></div>
-<main>
-	<article class="page">
-		<header>
-			<h1>Error 404: Page not found</h1>
-		</header>
-		<p>Oooh, snap! Seems like the page you are looking for does not exist. Perhaps you misspelled it?</p>
-		<p>If you are developing this site, try enabling the output of detailed messages to get some more specific info.</p>
-	</article>
-</main>
+<article class="page">
+	<header>
+		<h1>Error 404: Page not found</h1>
+	</header>
+	<p>Oooh, snap! Seems like the page you are looking for does not exist. Perhaps you misspelled it?</p>
+	<p>If you are developing this site, try enabling the output of detailed messages to get some more specific info.</p>
+</article>
