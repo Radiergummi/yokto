2dd437e3482dd0cebad273a6c10fab135558c652
Mo <Radiergummi@users.noreply.github.com>
2015-06-09 18:32:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,17 @@
-<?
-/**
- * yocto
- *
- * minimalist website framework designed for the most basic web apps and sites
- *
- * @package		yocto
- * @link		http://moritzfriedrich.com
- * @copyright	http://unlicense.org/
- */
+# Rewriting
+<IfModule mod_rewrite.c>
+RewriteEngine On
+RewriteBase /
+RewriteRule ^index\.php$ - [L,QSA]
 
-define('DS', DIRECTORY_SEPARATOR);
-define('PATH', dirname(__FILE__) . DS);
-define('SYS', PATH . 'system' . DS);
-define('EXT', '.php');
+# Rewrite all assets to get shorter links (like "/css/style.css")
+RewriteRule ^(css|js|img|icon)/(.*)$ /public/assets/$1/$2 [L,QSA]
 
-define('URL', (isset($_SERVER['HTTPS']) ? "https" : "http") . '://' . $_SERVER['HTTP_HOST']);
+# Rewrite all requests for real file paths
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
 
-require SYS . 'hooks' . EXT;
-require SYS . 'config' . EXT;
-require SYS . 'functions' . EXT;
-require SYS . 'router' . EXT;
-require SYS . 'parser' . EXT;
-require SYS . 'view' . EXT;
-require SYS . 'error' . EXT;
-require SYS . 'init' . EXT;
+# Rewrite all requests to index.php
+RewriteRule . /index.php [L,QSA]
+
+</IfModule>
